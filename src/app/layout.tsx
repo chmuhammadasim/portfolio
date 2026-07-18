a8b58eecc756bfdd968a38c8c67ab6ac1f056848
chmuhammadasim <muhammadasimchattha@gmail.com>
2026-07-18 14:34:16 +0500

    added google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -123,6 +123,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NT9HZ9VS');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <meta name="theme-color" content="#0a0a0f" />
         <meta name="color-scheme" content="dark" />
         <link rel="canonical" href="https://chmuhammadasim.site" />
@@ -201,6 +212,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0a0a0f] text-gray-200`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NT9HZ9VS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
